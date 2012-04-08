02fdd8dcaa110a5a968cdb38fd477e97f38ce2c8
jterry <jterry7484@gmail.com>
2012-04-08 09:45:32 -0500

    Pulled in Austen's project and consolidated all of the files back into one project.

---
+++ CS452 Project/FalconPoint4/FalconPoint4/FPdrawer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using fvw;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace FalconPoint4
+{
+    class FPdrawer
+    {
+        string iconLoc = null;
+        Logistics logistics = new Logistics();
+
+        public FPdrawer()
+        {
+
+            if (System.Environment.Is64BitOperatingSystem == true)
+                iconLoc = "C:\\Program Files (x86)\\PFPS\\falcon\\data\\icons\\Localpnt\\NFZ.ico";
+            else
+                iconLoc = "C:\\Program Files\\PFPS\\falcon\\data\\icons\\Localpnt\\NFZ.ico";
+
+        }
+
+
+        //Austen: changed input params to pass entire layer list so that we'd have access to all it's values
+        public void CreatePoint(ILayer FP_point, int layer, LayerList layerList, string id, double lat, double lon,string time)
+        {
+            try
+            {
+                FP_point.DeleteAllObjects(layer);// delete everything on this layer.. keeps us from having bread crumbs 
+              
+                FP_point.AddIcon(layer, iconLoc, lat, lon, id);
+
+                if (layerList.FP_LatLonlist.Count > 1)
+                {
+                    double lat1 = layerList.FP_LatLonlist[layerList.FP_LatLonlist.Count - 1].lat;
+                    double lon1 = layerList.FP_LatLonlist[layerList.FP_LatLonlist.Count - 1].lon;
+                    DateTime time1 = layerList.FP_LatLonlist[layerList.FP_LatLonlist.Count - 1].time;
+
+                    double heading = logistics.Heading(lat, lon, lat1, lon1); //Compute heading
+                    double speed = logistics.SpeedMPH(lat, lon, lat1, lon1, Convert.ToDateTime(time), time1); //Compute Speed
+                    RenderArrow(FP_point, layerList, lat, lon, time, heading);
+
+
+                    AddHeadingText(FP_point, layerList, lat, lon, heading);
+                    AddSpeedText(FP_point, layerList, lat, lon, speed);
+                }
+
+                FP_point.Refresh(-1);
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+                MessageBox.Show("Error Message: "+ex.Message+ " Stack Trace: "+ex.StackTrace);
+            }
+        }
+
+        public void RenderArrow(ILayer FP_point, LayerList layerList, double lat, double lon,string time,double heading)
+        {
+
+            FP_point.SetLineType(layerList.Layer, 104); //Doesn't do crap...
+        
+            int symbolHandle = FP_point.CreateSymbol("arrow", 0);  //Create symbol 0 = doesn't already exist
+
+            //add line to symbol from pixel 0,0 to 0,1 (use this to change where degree 0 is)
+            FP_point.AddLineToSymbol(symbolHandle, 0, 0, 0, 1);
+
+            //add created symbol to layer with lat long and rotation
+            FP_point.AddSymbol(layerList.Layer, symbolHandle, lat, lon, 20, heading);                                                    
+
+        }
+
+        public void AddSpeedText(ILayer FP_point, LayerList layerList, double lat, double lon, double speed)
+        {
+            FP_point.AddText(layerList.Layer, lat, lon, speed + " MPH", -13, -45);
+        }
+
+        public void AddHeadingText(ILayer FP_point, LayerList layerList, double lat, double lon, double heading)
+        {
+            FP_point.AddText(layerList.Layer, lat, lon, heading + "°", -13, -35); // -13,-35 sets position of text
+        }
+
+
+    }
+}
---
+++ CS452 Project/FalconPoint4/FalconPoint4/Logistics.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FalconPoint4
+{
+    class Logistics
+    {
+
+
+        public double Heading(double lat1, double lon1,double lat2, double lon2)
+        {
+           
+            double dLon = deg2rad(lon2 - lon1);
+            double dPhi = Math.Log(Math.Tan(deg2rad(lat2) / 2 + Math.PI / 4) / Math.Tan(deg2rad(lat1) / 2 + Math.PI / 4));
+
+            if (Math.Abs(dLon) > Math.PI)
+            {
+                dLon = dLon > 0 ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
+            }
+
+            return Math.Truncate(ToHeading(Math.Atan2(dLon, dPhi)));
+
+        }
+
+        public double SpeedMPH(double lat1, double lon1, double lat2, double lon2,DateTime time,DateTime time1)
+        {
+            TimeSpan ts = time - time1;
+          //  MessageBox.Show(Distance(lat1, lon1, lat2, lon2).ToString() + " " + ts.Hours);
+            return Math.Truncate((((((Distance(lat1, lon1, lat2, lon2) / (ts.Hours))/12)*60)*60)/5280)); //still don't know if this is right
+        }
+
+        public double ToHeading(double radians)
+        {
+            // convert radians to degrees (as heading: 0...360)
+            return (rad2deg(radians) + 360) % 360;
+        }
+
+            public double Distance (double lat1, double lat2, double lon1, double lon2)
+            {
+               // double lat1 = 34.713496;
+               // double lat2 = 34.713538;
+               // double lon1 = -86.685663;
+               // double lon2 = -86.688520;
+
+                //double dlong = (lon2 - lon1) * 0.0174532925199433;
+                //double dlat = (lat2 - lat1) * 0.0174532925199433;
+                //double a = Math.Pow(Math.Sin(dlat / 2.0), 2) + Math.Cos(lat1 * 0.0174532925199433) * Math.Cos(lat2 * 0.0174532925199433) * Math.Pow(Math.Sin(dlong / 2.0), 2);
+                //double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                //double d = 3956 * c; 
+
+
+                double theta = lon1 - lon2;
+                double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
+                dist = Math.Acos(dist);
+                dist = rad2deg(dist);
+                dist = dist * 60 * 1.1515;
+                dist = dist * .8684;
+
+                return dist;
+            }
+
+            public double deg2rad(double deg)
+            {
+                return (deg * Math.PI / 180.0);
+            }
+
+            public double rad2deg(double rad)
+            {
+                return (rad / Math.PI * 180.0);
+            }
+    
+
+
+    }
+}
